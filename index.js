c0c8a349d209312ed836435e4ca4d83829188db8
park jae hyeon <janhyun99@naver.com>
2019-11-22 18:21:02 +0900

    login
    login

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var express = require('express');
 // Express 객체 생성
 var app = express();
+var http = require('http');
+var server = http.createServer(app);
 
 // ejs view와 렌더링 설정
 app.use(express.static('html_template'));
@@ -35,8 +37,30 @@ connection.connect(function (err) {
   console.log('Success DB connection');
 });
 
+// 좌석 예매 gui
+var socketio= require('socket.io');
+var fs = require('fs');
+var seats = [
+           [1,1,0,0,1,1],
+           [1,1,1,1,1,1],
+           [1,1,1,1,1,1]];
+
+
+app.get('/seats',
+  function(request,response)
+  {response.send(seats);});
+
+app.get('/',
+  function(request, response)
+  {
+fs.readFile('HTMLPage.html',function(error,data){
+      response.send(data.toString());
+        });
+  });
+
+
 // Express 서버 시작
-app.listen(3307, function () {
+server.listen(3307, function () {
     console.log('Example app listening on port 3306!');
 });
 
@@ -45,16 +69,77 @@ app.listen(3307, function () {
 app.get('/', function (req, res) {
     res.render('index.html');
 });
-//app.get('/register', function (req, res) {
-//    res.render('register.html');
-//});
-//app.post('/register', function (req, res) {
-//  var name = req.body.name;
-//  var pwd = req.body.pwd;
-//  var pwdconf = req.body.pwdconf;
-//  console.log(name, pwd);
-//});
-//app.get('/welcome', function (req, res) {
-//    res.render('welcome.html');
-//});
+var io =
+    socketio.listen(server);
+ io.sockets.on('connection',
+ function(socket){
+   socket.on('reserve',
+   	function(data)
+   	{ seats[data.y][data.x]=2;
+ io.sockets.emit('reserve', data);
+   	  });
+   socket.on('reserveBak',
+   function(data)
+   { seats[data.y][data.x]=1;
+ io.sockets.emit('reserveBak', data);
+      });
+   });
 
+   //로그인 구현
+   app.get('/', function(req, res) {
+    res.render('login.html');
+  });
+  
+  app.post('/', function(req, res){
+    var id = req.body.id;
+    var pwd = req.body.pwd;
+  
+    var sql = `SELECT * FROM members WHERE log_id = ?`;
+    connection.query(sql, [id], function(error, results, fields){
+      if(results.length == 0){
+
+        res.render('login.html');
+      }
+      else{
+        var db_name = results[0].log_id;  //'username'는 데이터베이스 칼럼 이름
+        var db_pwd = results[0].log_pw;  //'pwd'또한 데이터베이스 칼럼 이름
+
+        if(pwd == db_pwd){;
+          res.render('index.html');
+        }
+        else{
+          res.render('login.html');
+        }
+      }
+    });
+  });
+
+  // 회원가입 연동
+  app.get('/sign_up', function(req, res) {
+    res.render('sign_up.html');
+  });
+  
+  app.post('/sign_up', function(req, res){
+    var log_id = req.body.log_id;
+    var log_pw = req.body.log_pw;
+    var pwdconf = req.body.pwdconf;
+    var gender = req.body.gender;
+    var mem_id = req.body.mem_id;
+    var phone_num = req.body.phone_num;
+    var email = req.body.email;
+    if(log_pw === pwdconf){
+
+      //DB에 쿼리 알리기
+      var sql = `INSERT INTO members (mem_id,log_id,log_pw,pwdconf,gender,phone_num,email) VALUES(?, ?, ?, ?, ?, ?, ?)`;
+      connection.query(sql, [mem_id,log_id,log_pw,pwdconf,gender,phone_num,email], function(error, results, fields){
+        console.log(error);
+      });
+  
+      res.redirect('/');
+    }
+    else{
+      res.render(alert("비밀번호 오류"));
+      res.render('sign.html');
+    }
+  });
+module.exports = app;
