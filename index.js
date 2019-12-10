67db918f16b084e2b367e9dee4f60674d4deb85d
JEONGEUNJI <jeong-eunji@jeong-eunjiui-MacBookPro.local>
2019-12-11 05:15:42 +0900

    screen.ejs modified

--- index.js
+++ index.js
@@ -240,17 +240,24 @@ app.get('/movie_info', function(req, res) {
 
 });
 app.get('/screen', function(req, res) {
-  if (req.session.user) {
-    res.render('screen.ejs', {
-      logined : req.session.user.logined,
-      user_id : req.session.user.user_id
-    });
-  } else {
-    res.render('screen.ejs', {
-      logined : false,
-      user_id : null
-    });
-  }
+  var sql = `SELECT * FROM movie`;
+  connection.query(sql, function(error,results,fields){
+    console.log(results);
+     if (req.session.user) {
+      res.render('screen.ejs', {
+        logined : req.session.user.logined,
+        user_id : req.session.user.user_id,
+        results
+      });
+    } else {
+      res.render('screen.ejs', {
+        logined : false,
+        user_id : null,
+        results
+      });
+    }
+  });
+ 
 });
 app.get('/seat_ticket', function(req,res){
   res.render('seat_ticket.ejs');
